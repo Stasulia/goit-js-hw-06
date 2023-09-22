6ae64efa837dfe92815c97ab010c2dff971f6045
Stasulia <134652473+Stasulia@users.noreply.github.com>
2023-09-22 17:28:40 +0300

    task-01 is done

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,48 +1,14 @@
 
-const items = document.querySelectorAll('.item')
-console.log(items);
-const arrOfItem = Array.from(items)
-console.log(arrOfItem);
-function countCategories(arrOfItem) {
-    let totalCategories = arrOfItem.length;
-  return `Number of categories: ${totalCategories}`
- }
- console.log(countCategories(arrOfItem));
+const item = document.querySelectorAll('.item')
+const items = Array.from(item)
 
-// const title = document.querySelectorAll('h2')
-// console.log(title);
-// const arrOfElements = Array.from(title)
-// console.log(arrOfElements);
-function countElements(arrOfItem) {
-    let totalElements = 0;
-    arrOfItem.forEach(element => {
-        
-    });
-    return
+function countCategories(items) {
+  let totalCategories = items.length;
+  console.log(`Number of categories: ${totalCategories}`) 
+items.forEach(function(item) {
+    console.log(`Category : ${item.firstElementChild.textContent}\nElements : ${item.lastElementChild.children.length}` )
 }
-//const 
-// const totalCategories = function(arr) {
-//   arr.forEach(function(item) {
-//     totalCategories += item;
-// }
-// );
-// return `Number of categories: ${totalCategories}`
-// }
-// console.log(totalCategories());
-
-// function countCategories(arr) {
-//     let totalCategories = 0;
-//     arr.forEach(function(element) {
-//         totalCategories += element;
-//     } )
-//     return totalCategories;
-// }
-// console.log(countCategories());
- 
-// const countCategories = arr => {let totalCategories = 0;
-// arr.forEach(element => { totalCategories += element
-//     });
-//     return `Number of categories: ${totalCategories}`;
-// }
-
+)
+}  
+console.log(countCategories(items));
 
