774d3bca8b5cd5231cec60aaeccd56ec5bb179b7
Stasulia <134652473+Stasulia@users.noreply.github.com>
2023-09-25 15:12:03 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', handlerSubmit);
 
+function handlerSubmit(event) {
+ event.preventDefault();
+ const {email, password} = event.currentTarget.elements;
+ const data = {
+    email: email.value,
+    password: password.value,
+ };
+  if (email.value === '' || password.value === '') {
+  alert (`Треба заповнити всі поля`);
+  }
+ else {
+ console.log(data);
+}
+ event.currentTarget.reset();
+};
